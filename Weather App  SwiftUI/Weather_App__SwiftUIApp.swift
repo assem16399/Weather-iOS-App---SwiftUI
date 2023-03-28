069ba920cfa9d02dd08fa9b1fd73b2225460835e
assem16399 <aasem.hany@gmail.com>
2023-03-28 15:31:01 +0200

    Initial Commit

---
+++ Weather App  SwiftUI/Weather_App__SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App__SwiftUIApp.swift
+//  Weather App  SwiftUI
+//
+//  Created by Aasem Hany on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App__SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
